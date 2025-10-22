03788db51fd6c71363fbc18ada17d693a2d89a31
Thimira Sahan <161017879+SahanJDesigns@users.noreply.github.com>
2025-10-22 20:53:54 +0530

    Update config.py

--- backend/user-service/app/core/config.py
+++ backend/user-service/app/core/config.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb://mongo:27017"
-    MONGO_DB: str = "genealogy"
+    MY_MONGO_URI: str = "mongodb://mongo:27017"
+    MY_MONGO_DB: str = "genealogy"
     SECRET_KEY: str = "change_this_secret_in_prod"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24
